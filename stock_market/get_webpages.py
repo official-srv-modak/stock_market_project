6b6d66a40f253f94c9d79e9a7c1f022cdee560c8
souravmodak <souM#@583>
2020-11-18 14:49:33 +0530

    fetching nifty table

--- stock_market/get_webpages.py
+++ stock_market/get_webpages.py
@@ -15,6 +15,10 @@ from pprint import pprint
 from html_table_parser import HTMLTableParser
 import pandas as pd
 import os
+import re
+
+"""from nltk.corpus import wordnet as wn
+nouns = {x.name().split('.', 1)[0] for x in wn.all_synsets('n')}"""
 try:
     # For Python 3.0 and later
     from urllib.request import urlopen
@@ -46,10 +50,25 @@ def get_companies_table(query):
 
     output = search(query, lang='en', num=10, stop=10, pause=2)
     for url in output:
-        best_search_url = url
-        break
-    return get_table_data(best_search_url)
+        try:
+            table = get_table_data(url) #filter on the table variable
+            return table
+        except:
+            print("Sorry no table in the url... Trying another")
+            continue
 
+def filter_table(table):
+    """ This is to add filter to the table s that we dont pick up useless tables
+    It returns True or False depending upon if the table is accepted. Sometimes it can return the table
+    instead of a True which can work as fine for an if statement like :- if filter table :"""
+
+
+    for row in table:
+        for tuple in row[0:1]:
+            if tuple in nouns:
+                table = table[0:]
+
+    return False
 
 
 def main():
@@ -59,6 +78,22 @@ def main():
     result = performance_check(number_of_run)"""
 
     print("WELCOME!!! You can use this program to extract information and news on the stock market and table of companies")
+    print("1. Details on nifty companies")
+    print("2. Details on specific compnaies")
+    print("3. Exit")
+    val = input()
+    if val ==  "1":
+        table = get_companies_table("nifty companies")
+        tuple_cnt = 0
+        for tuple in table[0]:
+            if re.search("name", tuple) or re.search("NAME", tuple) or re.search("Name", tuple):
+                break
+            tuple_cnt += 1
+        list_of_companies = list()
+        for row in table[1:]:
+            list_of_companies.append(row[tuple_cnt])
+        print(list_of_companies)
+
 
 
 main()
